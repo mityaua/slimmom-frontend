2145a061c69c2df3c0b3847f5f39c05983a4eacd
Iliya <iliyamiroshnichenko88@gmail.com>
2021-07-24 09:36:32 +0300

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import Button from './components/Button';
 
 function App() {
-  return <div>Test test test stes </div>;
+  return (
+    <div>
+      Test test test stes
+      <Button text="Вход" type="primary" />
+      <Button text="Регистрация" type="secondary" />
+    </div>
+  );
 }
 
 export default App;
