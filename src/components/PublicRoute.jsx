53f0a9e62bc0e05933530f789dc37f0ee723b4aa
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-07-24 17:55:59 +0300

    Добавляет компоненты-заглушки для публичных и приватных роутов + сброс общих стилей

---
+++ src/components/PublicRoute.jsx
@@ -0,0 +1,23 @@
+// - Если маршрут ограниченный, и пользователь залогинен, рендерит редирект на указанный роут
+// - В противном случае рендерит компонент
+
+import { Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { authSelectors } from '../redux/auth';
+
+// Компонент публичного роута (перед внедрением подогнать под наш стейт!)
+const PublicRoute = ({ redirectTo, children, ...routeProps }) => {
+  const isLoggedIn = useSelector(authSelectors.getIsAuthenticated); // Селектор статуса аутентификации
+
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn && routeProps.restricted ? (
+        <Redirect to={redirectTo} />
+      ) : (
+        children
+      )}
+    </Route>
+  );
+};
+
+export default PublicRoute;
