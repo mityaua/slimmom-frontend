336c7c98f31e8a59811b3da6b7e4e8460f586290
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-07-25 18:16:29 +0300

    Исправляет хедер на странице регистрации

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -6,12 +6,14 @@ import styles from './RegistrationPage.module.css';
 
 const RegistrationPage = () => {
   return (
-    <Container>
+    <>
       <Header className={styles.registrationPage__header} />
-      <div className={styles.registrationPage}>
-        <RegistrationForm className={styles.registrationPage__form} />
-      </div>
-    </Container>
+      <Container>
+        <div className={styles.registrationPage}>
+          <RegistrationForm className={styles.registrationPage__form} />
+        </div>
+      </Container>
+    </>
   );
 };
 
