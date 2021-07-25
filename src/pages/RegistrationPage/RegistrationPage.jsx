5e1ae27d6369a8103a827c7967f67f3da49a2a81
Sergey Glazyrin <glazyrinsb@gmail.com>
2021-07-25 14:08:28 +0300

    layout Registration Page

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,7 +1,18 @@
+import Container from '../../components/Container';
+import Header from '../../components/Header';
+import RegistrationForm from '../../components/RegistrationForm';
+
 import styles from './RegistrationPage.module.css';
 
 const RegistrationPage = () => {
-  return <div className={styles.registrationPage}></div>;
+  return (
+    <Container>
+      <Header className={styles.registrationPage__header} />
+      <div className={styles.registrationPage}>
+        <RegistrationForm className={styles.registrationPage__form} />
+      </div>
+    </Container>
+  );
 };
 
 export default RegistrationPage;
