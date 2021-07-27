150dc0295550bad657558b9d7190a2f71a7ad534
Vita Khutorianska <steblvita@gmail.com>
2021-07-27 19:11:07 +0300

    add logout in userinfo.jsx

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,11 @@
 import styles from './UserInfo.module.css';
+import { logOut } from '../../redux/auth/auth_operation';
 
+import { useDispatch } from 'react-redux';
 const UserInfo = () => {
-  const onLogout = () => null;
+  const dispatch = useDispatch();
+  const onLogout = () => dispatch(logOut());
+  // const onLogout = () => null;
 
   return (
     <div className={styles.userInfo}>
