82ba3ad1b6e70963196e8f59e123ea5e71666d1d
Vita Khutorianska <steblvita@gmail.com>
2021-07-27 19:30:05 +0300

    fixx selector

--- src/redux/auth/auth_selector.js
+++ src/redux/auth/auth_selector.js
@@ -1,2 +1,2 @@
 export const getIsAuthenticated = state => state.auth.isLogged;
-export const getUserEmail = state => state.auth.user.email;
+export const getUserNickName = state => state.auth.user.name;
