209a98130573ffac78eee23372f201ea13439ed1
Iliya <iliyamiroshnichenko88@gmail.com>
2021-07-27 18:37:00 +0300

    add basic form structure and mobile version

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,8 +1,13 @@
 import styles from './Button.module.css';
 
-const Button = ({ text, type }) => {
-  const className = type === 'primary' ? styles.primary : styles.secondary;
-  return <button className={`${styles.button} ${className}`}>{text}</button>;
+const Button = ({ text, customType, type }) => {
+  const className =
+    customType === 'primary' ? styles.primary : styles.secondary;
+  return (
+    <button type={type} className={`${styles.button} ${className}`}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/DiaryAddProductForm/DiaryAddProductForm.jsx
+++ src/components/DiaryAddProductForm/DiaryAddProductForm.jsx
@@ -1,7 +1,87 @@
 import styles from './DiaryAddProductForm.module.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '../Container';
+import TextField from '@material-ui/core/TextField';
+import Button from '../Button';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
+const useStyles = makeStyles({
+  input: {
+    width: '100%',
+  },
+  nameInput: {
+    marginBottom: 30,
+  },
+  weightInput: {
+    marginBottom: 60,
+  },
+});
+
+const validationSchema = yup.object({
+  productName: yup.string('Enter product').required('Product is required'),
+  productWeight: yup
+    .number('Enter number')
+    .min(10, 'Enter more gramms')
+    .max(1000, 'Enter less gramms')
+    .required('Weight is required'),
+});
 
 const DiaryAddProductForm = () => {
-  return <div className={styles.diaryAddProductForm}></div>;
+  const classes = useStyles();
+
+  const formik = useFormik({
+    initialValues: {
+      productName: '',
+      productWeight: '',
+    },
+    validationSchema: validationSchema,
+
+    onSubmit: values => {
+      const payload = {
+        productName: values.productName,
+        productWeight: values.productWeight,
+      };
+
+      console.log(payload);
+    },
+  });
+  return (
+    <div className={styles.diaryAddProductForm}>
+      <Container>
+        <form className={styles.form} onSubmit={formik.handleSubmit}>
+          <TextField
+            className={`${classes.input} ${classes.nameInput}`}
+            id="productName"
+            name="productName"
+            value={formik.values.productName}
+            placeholder="Введите название продукта"
+            onChange={formik.handleChange}
+            error={
+              formik.touched.productName && Boolean(formik.errors.productName)
+            }
+            helperText={formik.touched.productName && formik.errors.productName}
+          />
+          <TextField
+            className={`${classes.input} ${classes.weightInput}`}
+            id="productWeight"
+            name="productWeight"
+            value={formik.values.productWeight}
+            placeholder="Граммы"
+            onChange={formik.handleChange}
+            error={
+              formik.touched.productWeight &&
+              Boolean(formik.errors.productWeight)
+            }
+            helperText={
+              formik.touched.productWeight && formik.errors.productWeight
+            }
+          />
+          <Button text="Добавить" customType="primary" type="submit" />
+        </form>
+      </Container>
+    </div>
+  );
 };
 
 export default DiaryAddProductForm;
