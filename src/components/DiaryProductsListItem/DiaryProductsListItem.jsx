7cb9495db7be8ba7711abf78751ee002a84a1385
vetallatka <latka.vitalik@yandex.ru>
2021-07-29 00:49:22 +0200

    fixed CloseIcon

--- src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
+++ src/components/DiaryProductsListItem/DiaryProductsListItem.jsx
@@ -7,53 +7,65 @@ const DiaryProductsListItem = () => {
     <div>
       <div className={styles.item}>
         <div className={styles.name}>Баклажан</div>
-        <div className={styles.weight}>100 г</div>
+        <div className={styles.weight} placeholder="съедено">
+          100 г
+        </div>
         <div className={styles.calories}>
           <span>320 ккал</span>
-          <span>
-            <CloseIcon />
-          </span>
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
       <div className={styles.item}>
         <div className={styles.name}>Мясо птицы</div>
         <div className={styles.weight}>100 г</div>
         <div className={styles.calories}>
           <span>480 ккал</span>
-          <CloseIcon />
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
       <div className={styles.item}>
         <div className={styles.name}>Хлеб</div>
         <div className={styles.weight}>100 г</div>
         <div className={styles.calories}>
           <span>200 ккал</span>
-          <CloseIcon />
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
       <div className={styles.item}>
         <div className={styles.name}>Орех </div>
         <div className={styles.weight}>100 г</div>
         <div className={styles.calories}>
           <span>205 ккал</span>
-          <CloseIcon />
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
       <div className={styles.item}>
         <div className={styles.name}>Мясо свинное</div>
         <div className={styles.weight}>100 г</div>
         <div className={styles.calories}>
           <span>580 ккал</span>
-          <CloseIcon />
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
       <div className={styles.item}>
         <div className={styles.name}>Картофель</div>
         <div className={styles.weight}>100 г</div>
         <div className={styles.calories}>
           <span>380 ккал</span>
-          <CloseIcon />
         </div>
+        <button className={styles.btn}>
+          <CloseIcon />
+        </button>
       </div>
     </div>
   );
