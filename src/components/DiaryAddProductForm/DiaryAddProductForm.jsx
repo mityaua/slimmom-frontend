bf530ed617e30b68e8a96dda8aef25d2dfbbfc73
Iliya <iliyamiroshnichenko88@gmail.com>
2021-07-29 14:49:10 +0300

    add mobile DiaryPage

--- src/components/DiaryAddProductForm/DiaryAddProductForm.jsx
+++ src/components/DiaryAddProductForm/DiaryAddProductForm.jsx
@@ -32,15 +32,15 @@ const useStyles = makeStyles({
 });
 
 const validationSchema = yup.object({
-  productName: yup.string('Enter product').required('Product is required'),
+  productName: yup.string('Enter product').required('Продукт обязателен!'),
   productWeight: yup
-    .number('Enter number')
-    .typeError('Enter number')
+    .number('Введите число')
+    .typeError('Введите число')
     .positive()
     .integer()
-    .min(10, 'Enter more gramms')
-    .max(1000, 'Enter less gramms')
-    .required('Weight is required'),
+    .min(10, 'Введите больший вес')
+    .max(1000, 'Введите меньший вес')
+    .required('Вес обязателен!'),
 });
 
 const DiaryAddProductForm = () => {
@@ -92,7 +92,7 @@ const DiaryAddProductForm = () => {
             formik.touched.productWeight && formik.errors.productWeight
           }
         />
-        <Button customType="primary" type="submit" className="small">
+        <Button customType="primary" type="submit" className="smallFromTablet">
           <span className={styles.hiddenIcon}>
             <AddIcon />
           </span>
