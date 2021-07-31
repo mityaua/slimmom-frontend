a8a9a18ec61d51c6e2b693653ab6c7615a94e0eb
Ivan Kuchvara <ivan.kuchvara@gmail.com>
2021-07-31 12:22:02 +0300

    modal and backdrop fixes

--- src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ src/components/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -12,12 +12,14 @@ import {
 
 import Button from '../Button';
 import Modal from '../Modal';
+import routes from '../../routes';
 
 import { fetchDailyCalories } from '../../redux/dailyCalories/dailyCalories_operation';
 import { getIsAuthenticated } from '../../redux/auth/auth_selector';
 
 import styles from './DailyCaloriesForm.module.css';
 import { withStyles } from '@material-ui/core/styles';
+import { NavLink } from 'react-router-dom';
 
 const DailyCaloriesForm = () => {
   const dispatch = useDispatch();
@@ -240,26 +242,15 @@ const DailyCaloriesForm = () => {
             <li className={styles.modal_el}>4. Копчености</li>
           </ul>
           <div className={styles.modal_button}>
-            <Button text="Начать худеть" type="primary" />
-          </div>
+            <NavLink to={routes.registration} >
+              <Button text="Начать худеть" customType="primary" type='button'/>
+            </NavLink>
+          </div>                    
           <button
             type="button"
             className={styles.modal_closeBtn}
             onClick={toggleModal}
-          >
-            <svg
-              width="12"
-              height="12"
-              viewBox="0 0 12 12"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M11.8333 1.3415L10.6583 0.166504L5.99998 4.82484L1.34164 0.166504L0.166641 1.3415L4.82498 5.99984L0.166641 10.6582L1.34164 11.8332L5.99998 7.17484L10.6583 11.8332L11.8333 10.6582L7.17498 5.99984L11.8333 1.3415Z"
-                fill="black"
-              />
-            </svg>
-          </button>
+          ></button>
         </Modal>
       )}
     </>
