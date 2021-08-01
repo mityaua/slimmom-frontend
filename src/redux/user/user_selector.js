7780c7ca834479ac5d0edf4ac580b6da53d10d02
Dmitriy Shukaliuk <mityaua@gmail.com>
2021-08-01 15:39:31 +0300

    Переносит селекторы в юзера

--- src/redux/user/user_selector.js
+++ src/redux/user/user_selector.js
@@ -1,2 +1,3 @@
 export const getDays = state => state.user.userInfo.days;
 export const getUserId = state => state.user.userInfo._id;
+export const getUserNickName = state => state.user.userInfo.name;
