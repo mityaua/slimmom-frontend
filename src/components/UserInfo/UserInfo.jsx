344128147189ed5ab9909eeeeb8f58798450432c
Iliya <iliyamiroshnichenko88@gmail.com>
2021-08-02 22:02:42 +0300

    reset day data after logout v2

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,7 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserNickName } from '../../redux/user/user_selector';
 import { logOut } from '../../redux/auth/auth_operation';
-import { resetDayInfo } from '../../redux/day/day_operation';
 
 // import UserAvatar from '../UserAvatar';
 
@@ -10,19 +9,13 @@ import styles from './UserInfo.module.css';
 const UserInfo = () => {
   const dispatch = useDispatch();
   const onLogout = () => dispatch(logOut());
-  const reset = () => dispatch(resetDayInfo());
   const Nickname = useSelector(getUserNickName);
-
-  const handleClick = () => {
-    onLogout();
-    reset();
-  };
 
   return (
     <div className={styles.userInfo}>
       {/* <UserAvatar name={Nickname} size={'50px'} /> */}
       <span className={styles.userNickname}>{Nickname}</span>
-      <button className={styles.button} type="button" onClick={handleClick}>
+      <button className={styles.button} type="button" onClick={onLogout}>
         Выйти
       </button>
     </div>
