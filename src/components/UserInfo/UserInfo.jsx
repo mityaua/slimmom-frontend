1f9ba0404dc0f785c8d0968f0f233b7b774ee633
yarymaks <yarygin45@gmail.com>
2021-08-05 10:48:40 +0200

    avatar

--- src/components/UserInfo/UserInfo.jsx
+++ src/components/UserInfo/UserInfo.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getUserNickName } from '../../redux/user/user_selector';
 import { logOut } from '../../redux/auth/auth_operation';
@@ -10,12 +11,17 @@ const UserInfo = () => {
   const dispatch = useDispatch();
   const onLogout = () => dispatch(logOut());
   const Nickname = useSelector(getUserNickName);
+  const [size, setSize] = useState(window.innerWidth < 767 ? '30px' : '40px');
+
+  window.onresize = () => {
+    return setSize(window.innerWidth < 767 ? '30px' : '40px');
+  };
 
   return (
     <div className={styles.userInfo}>
       <div className={styles.userAvatarBlock}>
         <div className={styles.userAvatar}>
-          <UserAvatar name={Nickname} size={'40px'} />
+          <UserAvatar name={Nickname} size={size} />
         </div>
       </div>
       <span className={styles.userNickname}>{Nickname}</span>
